b514867a7ecd98c960b3573d07648abbf7833ddb
SalahMohamed <>
2022-10-18 22:18:09 +0300

    no message

---
+++ TextFieldKitSwiftUIExample/SecondContentView/SecondContentView.swift
@@ -0,0 +1,81 @@
+//
+//  SecondContentView.swift
+//  TextFieldKitSwiftUIExample
+//
+//  Created by Salah on 10/4/22.
+//
+
+import SwiftUI
+import TextFieldKit
+
+struct SecondContentView: View {
+    func fullName()->Binding<String> {
+     return .init(
+        get: {viewModel.fullName },
+        set: {viewModel.fullName = $0 }
+    )
+    }
+    func firstName()->Binding<String> {
+     return .init(
+        get: {viewModel.firstName },
+        set: {viewModel.firstName = $0 }
+    )
+    }
+    func username()->Binding<String> {
+     return .init(
+        get: {viewModel.userName },
+        set: {viewModel.userName = $0 }
+    )
+    }
+    func requirements()->Binding<String> {
+     return .init(
+        get: {viewModel.requirements },
+        set: {viewModel.requirements = $0 }
+    )
+    }
+    @StateObject var viewModel = SecondContentViewModel.init()
+
+    var body: some View {
+        ZStack{
+            VStack(spacing:0) {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                SUITextFieldView(placeholder: nil,
+                                 text:username(),
+                                 error:$viewModel.userNameError,
+                                 iconName:"ic_userOnlineTutor",
+                                 onEditingChanged:nil,
+                                 validation:(Username(),self.viewModel.onEditingChangedUsername()))
+                SUITextFieldView(placeholder: nil,
+                                 text:fullName(),
+                                 error:$viewModel.fullNameError,
+                                 onEditingChanged:nil,
+                                 validation:(FullName(),self.viewModel.onEditingChangedFullName()))
+                SUITextFieldView(placeholder:nil,
+                                 text:firstName(),
+                                 error:$viewModel.firstNameError,
+                                 onEditingChanged:nil,
+                                 validation:(FirstName(),self.viewModel.onEditingChangedFirstName()))
+                SUITextViewView.init(placeholder: nil,
+                                     text:requirements(),
+                                     error:$viewModel.requirementsError,
+                                     iconName:"ic_userOnlineTutor",
+                                     onEditingChanged:nil,
+                                     validation:(Requirements(),self.viewModel.onEditingChangedRequirements()))
+//                Button.init(action: self.viewModel.save(), label:{Text("Save")})
+            }
+            .padding()
+        }.frame(maxWidth:.infinity,maxHeight:.infinity).background(Color.gray.opacity(0.5)).gesture(TapGesture()
+            .onEnded { _ in
+                UIApplication.shared.endEditing();
+            })
+    }
+}
+
+struct SecondContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondContentView()
+    }
+}
---
+++ TextFieldKitSwiftUIExample/SecondContentView/SecondContentViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  SecondContentViewModel.swift
+//  TextFieldKitSwiftUIExample
+//
+//  Created by Salah on 10/8/22.
+//
+
+import UIKit
+import SwiftUI
+import TextFieldKit
+class SecondContentViewModel:NSObject,ObservableObject{
+    @Published  var fullName:String="Ahmed"
+    @Published  var firstName:String="Ahmed"
+    @Published  var userName:String="Ahmed"
+    @Published  var requirements:String=""
+
+    @Published  var fullNameError:String=""
+    @Published  var firstNameError:String=""
+    @Published  var userNameError:String=""
+    @Published  var requirementsError:String=""
+    override init() {
+        super.init();
+        
+    }
+    func save()->(()->Void){
+        return {
+            
+        }
+    }
+    func onEditingChangedFullName()->OnEditingValiadtionChanged{
+        return { value,validation  in
+            self.fullNameError = validation?.messages.string ?? "";
+        }
+    }
+    func onEditingChangedFirstName()->OnEditingValiadtionChanged{
+        return { value,validation in
+            self.firstNameError = validation?.messages.string ?? "";
+        }
+    }
+    func onEditingChangedUsername()->OnEditingValiadtionChanged{
+        return { value,validation in
+            self.userNameError = validation?.messages.string ?? "";
+        }
+    }
+    func onEditingChangedRequirements()->OnEditingValiadtionChanged{
+        return { value,validation in
+            self.requirementsError = validation?.messages.string ?? "";
+        }
+    }
+}
